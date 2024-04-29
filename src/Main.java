769395e1f0dfc2be086131f8ed81e54a79d4b9bf
DarkTempest <jorgeandresmartinezsantos@gmail.com>
2024-04-29 15:09:55 -0500

    Agregue un bucle for con numero.

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Buenas tardes como me le va.");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Numero: " + i);
+        }
+
     }
 }
