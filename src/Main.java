2ea9de9c027bbb60b8f7f064ae7c1a3991b5556a
GeorgeAMS <165567537+GeorgeAMS@users.noreply.github.com>
2024-05-14 11:11:18 -0500

    Se cambio agrego algo nuevo

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Bienvenido al menu");
-
+        System.out.pintln("Hola como tas")
 
         lista lista = new lista();
         lista.agregarAlPrincipio(3);
